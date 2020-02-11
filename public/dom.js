74642a08853aeb8d5b59631fc067b684aa314d54
rawan <rawan.designer@hotmail.com>
2020-02-11 16:08:52 +0200

    Adding the structure of the file

--- public/dom.js
+++ public/dom.js
@@ -1,16 +1,19 @@
-const request = (url, data) => {
-  const xhr = new XMLHttpRequest();
-  xhr.onreadystatechange = () => {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      const obj = JSON.parse(xhr.responseText);
-    }
+(() => {
+  const request = (url, data) => {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const obj = JSON.parse(xhr.responseText);
+      }
+    };
+    xhr.open('POST', url, true);
+    xhr.send(data);
   };
-  xhr.open('POST', url, true);
-  xhr.send(data);
-};
-const querySearch = document.querySelector('.query-search');
-querySearch.addEventListener('keyup', () => {
-  const inputValue = querySearch.value;
-  const url = '/book';
-  request(url, inputValue);
-});
+
+  const querySearch = document.querySelector('.query-search');
+  querySearch.addEventListener('keyup', () => {
+    const inputValue = querySearch.value;
+    const url = '/books';
+    request(url, inputValue);
+  });
+})();
